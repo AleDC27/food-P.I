9419af1d64d1314c81a272717296a50ce72519ab
AleDC27 <walteralejandrogordillo@gmail.com>
2023-04-17 14:15:59 -0300

    asd

--- api/src/db.js
+++ api/src/db.js
@@ -11,7 +11,9 @@ const {
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
  */
-const sequelize = new Sequelize( DB_DEPLOY, {
+const sequelize = new Sequelize( 
+  DB_DEPLOY, 
+  {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
