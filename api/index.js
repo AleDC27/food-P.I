e2b7f2cf9c05814ec99e865758f5a0acbc2cada7
AleDC27 <walteralejandrogordillo@gmail.com>
2023-04-17 15:24:59 -0300

    PORT

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@ const {
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(process.env.SERVER_PUERTO, () => {
+  server.listen(PORT, () => {
     console.log('%s listening at:'+ PORT); // eslint-disable-line no-console
   });
 });
